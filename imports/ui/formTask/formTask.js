9ead7a9fcc445767ea20fe08a03609acc2df2ed9
Antoine Chouinard <antoine.brochu@me.com>
2016-04-18 22:08:03 -0400

    Deuxième tentative
    Pour tester le tout

--- imports/ui/formTask/formTask.js
+++ imports/ui/formTask/formTask.js
@@ -6,10 +6,7 @@ import {moment} from 'moment';
 import './formTask.html';
 
 Template.formTask.events({
-//  'click input.checkbox' () {
-//    // Set the checked property to the opposite of its current value
-//    console.log("This is how we roll");
-//  },
+
   'click .delete' () {
     Tasks.remove(this._id);
   },
--- imports/ui/navigation/navigation.js
+++ imports/ui/navigation/navigation.js
@@ -1,14 +1,28 @@
-import {Template} from 'meteor/templating';
+import {
+  Template
+}
+from 'meteor/templating';
 
 //import { Tasks } from '../api/tasks.js';
 
 import './navigation.html';
 
+Template.navigation.events({
+  '#wrapper onload': function (e) {
+     $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+  });
+  }
+});
+
+Template.navigation.events = function () {
+
+}
 
 Template.navigation.events({
   'click .dropdown-menu' () {
     // Set the checked property to the opposite of its current value
-   console.log("This is how we roll");
+    console.log("This is how we roll");
   },
   'click .delete' () {
     Tasks.remove(this._id);
